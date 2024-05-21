b8075e127366eba966c7e437d46dc43328577289
Omkar Chougule <omkarchougule9545@gmail.com>
2024-05-21 15:21:37 +0530

    done with adding substitute doctors in frontend

--- client/src/pages/Substitute.jsx
+++ client/src/pages/Substitute.jsx
@@ -27,8 +27,28 @@ export const Substitute = () => {
             }
         } catch (error) {
             // next(error);
-            console.log(error)
+            // console.log(error)
             toast.error("Some thing went wrong")
+        }
+    }
+
+    const addSubstitute = async (id) => {
+        try {
+            const obj = { userId: id };
+            const response = await fetch(`http://localhost:5000/api/auth/${params.id}/substitute-doctors`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                },
+                body: JSON.stringify(obj)
+            });
+            // console.log(response);
+            if (response.ok) {
+                toast.success("Added Successfully");
+            }
+        } catch (error) {
+            toast.error("Something went wrong")
         }
     }
 
@@ -54,7 +74,7 @@ export const Substitute = () => {
                             <div className="card" key={index}>
                                 <p><b>Dr.</b> {firstName} {lastName}</p>
                                 <p><b>Timings:</b> {startTime} - {endTime}</p>
-                                <button>Add Substitute</button>
+                                        <button onClick={() => { addSubstitute(curDoc.userId) }}>Add Substitute</button>
                             </div>
                             </>
                             )
